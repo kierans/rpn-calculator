164a446fc5e7ac2ddf335c9e07c31a46b65c8b76
Kieran Simpson <kierans777@gmail.com>
2018-11-24 01:41:55 +1100

    Update main class to run the calculator.

--- src/main/java/org/quasar/rpn/RpnCalculator.java
+++ src/main/java/org/quasar/rpn/RpnCalculator.java
@@ -1,7 +1,34 @@
 package org.quasar.rpn;
 
+import java.util.Scanner;
+
+/**
+ * Very simple Reverse Polish Notation calculator.
+ */
 public class RpnCalculator {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Scanner scanner = new Scanner(System.in);
+    final Lexer lexer = new Lexer();
+    final Calculator calculator = new Calculator();
+    final CalculatorReporter reporter = new CalculatorReporter();
+
+    /*
+     * This sort of problem is more suited to a Event Driven/Reactive/Streaming approach.
+     * This is the simplest solution, but wouldn't be suited to a high volume environment.
+     */
+    while (true) {
+      try {
+        lexer.tokenise(scanner.nextLine()).forEach(calculator::push);
+      }
+      catch (InsufficientOperatorParametersException e) {
+        System.out.println(reporter.format(e));
+      }
+      catch (InvalidInputException e) {
+        System.out.println(reporter.format(e));
+      }
+      finally {
+        System.out.println(reporter.format(calculator.getState()));
+      }
+    }
   }
 }
